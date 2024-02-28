da84b9100418e84e720b635bcd1a5b2fc6102734
Fiofio <pink.is.swell@gmail.com>
2024-02-27 22:28:53 -0800

    studio3

---
+++ studio3/script.js
@@ -0,0 +1,20 @@
+(function(){
+    "use strict";
+    console.log("reading js");
+
+
+    document.getElementById('start').addEventListener('click', function() {
+        document.getElementById('overlay').classList.remove('hidden');
+    });
+    
+    document.getElementById('x').addEventListener('click', function() {
+        document.getElementById('overlay').classList.add('hidden');
+    });
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            document.getElementById('overlay').className='hidden';
+        }
+    });
+    
+})();
